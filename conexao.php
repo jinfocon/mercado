0ee96144aefdeec9b0250c29095e9cbe72e2c768
jean.carlos <jean.carlos@premiumtec.com.br>
2023-05-21 11:17:24 -0300

    Atualizado Informações do Banco

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "bdhost0038.servidorwebfacil.com";
-$username = "jinfocon_mercado";
-$password = "Sh_2023brsky";
-$database = "jinfocon_mercado";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "mercado";
